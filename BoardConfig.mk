96746f2df4e0020754498b838859b8d50ba48ea0
nycbjr <nycbjr@gmail.com>
2013-04-29 15:27:30 -0400

    fixing kernel build, adding hardware

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,9 +39,8 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 # Try to build the kernel
+TARGET_KERNEL_SOURCE := kernel/lge/mako
 TARGET_KERNEL_CONFIG := faux_mako_defconfig
-
-#TARGET_USE_PREBUILT_KERNEL := true
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_FLUENCE_INCALL := true
